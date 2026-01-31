d7bb86cc9b373c498d9f45e63a90066453efa3a7
ThyuHtooAungOwO <thyuhtoo7@gmail.com>
2026-01-31 12:20:57 +0630

    update CORS for production

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://interactive-comments-section-k469.vercel.app/',
+    origin: 'https://interactive-comments-section-k469.vercel.app',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   });
